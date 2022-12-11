e5486ae0fe57e47f509ed56fadd6a968c2c0fa91
GoncaloPeixoto <goncalopeixoto03@gmail.com>
2022-12-11 17:08:45 +0000

    inicio
    inicio do projeto

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor() {}
+
+  OpenLI(url:string){
+    window.open(url,'_system','location=yes')
+  }
+
+}
