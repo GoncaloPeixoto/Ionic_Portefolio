b02170d5785e1dbdc8268ae8bb9c1d851bded042
GoncaloPeixoto <goncalopeixoto03@gmail.com>
2022-12-20 13:11:31 +0000

    i don't remember what i did

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  segmentChanged(ev: any) {
+    console.log('Segment changed', ev);
+  }
 
+  constructor() {}
 }
